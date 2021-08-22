KS, KL distance corrected seems to be working well

drift_detection/_KLAdWin.py
@@ -1,55 +1,49 @@
 from ._ADWIN import ADWIN
 import numpy as np
 import random
+from scipy.integrate import simpson
+from scipy.stats import gaussian_kde
 
 class KLAdWin(ADWIN):
-    def __init__(self, n_bins = 20, delta = 1.5) -> None:
+    def __init__(self, n_bins = 50, delta = 0.1) -> None:
         """
         n_bins for creating probabilty distibution functions
         """
-        super().__init__(delta=delta)
+        super().__init__(delta=delta, minimum_window_size=300)
         self.n_bins = n_bins
+        self.kl = []
 
     def test_stat(self, window1, window2):
-        pdf1, pdf2 = self.create_pdf(window1,window2)
-        return self.calc_kl_distance(pdf1,pdf2)
+        pdf1, pdf2, x = self.create_pdf(window1,window2)
+        kl = self.calc_kl_distance(pdf1,pdf2,x)
+        self.kl.append(kl)
+        return kl
     
     @staticmethod
-    def calc_kl_distance(d1,d2):
-        return np.sum(np.multiply(d1, np.log(d1/d2)))
+    def calc_kl_distance(pdf1, pdf2, x):
+        combined = np.multiply(pdf2, np.log2(pdf2/pdf1))
+        integral = simpson(combined, x=x, dx=1)
+        return integral
 
     def create_pdf(self, window1:list, window2:list):
         """
-        Arguments: 
-        bootstraped data from dist 0 and dist 1 : list(s)
-
-        Returns: Two probabilty distrbutions that have the same bins
+        Returns: Two PDFs from the window with the same format
         for KL distance calculations : list(s)
         """
-        def generate_dist(n_bins, bin_width, dist, min_val):
-            # For every data point, to avoid div0 add 0.000000001
-            binned = np.zeros(n_bins)+0.00000000001
-            for i in dist:
-                r = i%bin_width
-                bin_index = int((i-r-min_val)/bin_width)-1
-                binned[bin_index] += 1
-            # Div by total number to get prob dist
-            total_n = np.sum(binned)
-            prob_dist = binned/total_n
-            return prob_dist
+        dist_1_kde = gaussian_kde(window1)
+        dist_2_kde = gaussian_kde(window2)
 
         all_vals = window1 + window2
-
-        # Get max and min from all vals
-        max_val = max(all_vals)
         min_val = min(all_vals)
+        max_val = max(all_vals)
 
-        bin_width = (max_val-min_val)/(self.n_bins)
+        x_eval = np.linspace(min_val, max_val, self.n_bins)
 
-        p_dist0 = generate_dist(n_bins=self.n_bins, bin_width=bin_width, dist=window1, min_val=min_val)
-        p_dist1 = generate_dist(n_bins=self.n_bins, bin_width=bin_width, dist=window2, min_val=min_val)
+        # Create Prob Density Fns
+        dist_1_pdf = dist_1_kde.pdf(x_eval)
+        dist_2_pdf = dist_2_kde.pdf(x_eval)
 
-        return p_dist0,p_dist1
+        return dist_1_pdf, dist_2_pdf, x_eval
 
     def get_bootstrap_intervals(self, dist0, dist1):
         """